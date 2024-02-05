50d6d246d783db91d5af6d3dc7d00914f0c06eff
Michael Brusegard <michbrus@stud.ntnu.no>
2024-02-05 14:45:49 +0100

    fix: :bug: removed english locale and fixed nav max width

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,21 +1,18 @@
 import { type Pathnames } from 'next-intl/navigation';
 
-const locales = ['en', 'no'];
+const locales = ['no'];
 const defaultLocale = 'no';
 const localePrefix = 'as-needed';
 
 const pathnames = {
   '/': '/',
   '/[destination]': {
-    en: '/[destination]',
     no: '/[destination]',
   },
   '/signin': {
-    en: '/signin',
-    no: '/logginn',
+    no: '/logg-inn',
   },
   '/profile': {
-    en: '/profile',
     no: '/profil',
   },
 } satisfies Pathnames<typeof locales>;
