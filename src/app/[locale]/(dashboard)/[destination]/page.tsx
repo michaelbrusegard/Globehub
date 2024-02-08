16d5932dba9201d0d8fa39aa105dc772d5316121
Eivind Holmen <eivinbho@ntnu.no>
2024-02-08 11:53:28 +0100

    Designed Login page

--- src/app/[locale]/(dashboard)/[destination]/page.tsx
+++ src/app/[locale]/(dashboard)/[destination]/page.tsx
@@ -1,6 +1,6 @@
 import { unstable_setRequestLocale } from 'next-intl/server';
 
-export default function Destination({
+export default function LogIn({
   params: { locale },
 }: {
   params: { locale: string };
@@ -8,7 +8,82 @@ export default function Destination({
   unstable_setRequestLocale(locale);
   return (
     <div>
-      <h1>Destinaion</h1>
+      <h1>Login page:</h1>
+      <form>
+        <div style={{ marginBottom: '10px' }}></div>
+
+        <div>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            id='username'
+            placeholder='Enter your username'
+            required
+            style={{
+              padding: '10px',
+              border: '1px solid #ccc', // Border style
+              borderRadius: '5px', // Rounded corners
+              marginBottom: '15px', // Margin bottom for spacing
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            placeholder='Enter your password'
+            required
+            style={{
+              padding: '10px',
+              border: '1px solid #ccc', // Border style
+              borderRadius: '5px', // Rounded corners
+              marginBottom: '15px', // Margin bottom for spacing
+            }}
+          />
+        </div>
+
+        <div style={{ marginBottom: '10px' }}></div>
+
+        <button
+          type='submit'
+          style={{
+            backgroundColor: '#4CAF50',
+            border: '2px solid #4CAF50', // Border style
+            color: 'white',
+            padding: '15px 32px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            margin: '4px 2px',
+            cursor: 'pointer',
+            borderRadius: '5px',
+          }}
+        >
+          Login
+        </button>
+
+        <button
+          type='submit'
+          style={{
+            backgroundColor: '#4CAF50',
+            border: '2px solid #4CAF50', // Border style
+            color: 'white',
+            padding: '15px 32px',
+            textAlign: 'center',
+            textDecoration: 'none',
+            display: 'inline-block',
+            fontSize: '16px',
+            margin: '4px 2px',
+            cursor: 'pointer',
+            borderRadius: '5px',
+          }}
+        >
+          Ikke registrert? Registrer deg her.
+        </button>
+      </form>
     </div>
   );
 }
