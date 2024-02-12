a70e3f7fe1b9da9bc22d82dd980e1ce4a2dee994
Rikke Schjonsby <rikkees@stud.ntnu.no>
2024-02-12 14:43:50 +0100

    feat: added profile avatar, review list and edit page to profile page. > > Co-authored-by: Martine Engebretsen <msengebr@stud.ntnu.no>

--- src/app/[locale]/(dashboard)/profile/page.tsx
+++ src/app/[locale]/(dashboard)/profile/page.tsx
@@ -1,8 +1,25 @@
-import { Button, Divider, Avatar, Input, Link } from '@nextui-org/react';
+import { Avatar, Button, Link } from '@nextui-org/react';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
+const reviews = [
+  {
+    dest: 'Milano',
+    text: "Min ferie i Milano var fantastisk! Byen pulserte av liv og energi, og jeg ble overveldet av den imponerende arkitekturen og kulturelle rikdommen. Shoppingopplevelsene var en drøm med luksuriøse butikker og trendy boutiques. Maten var en høydepunkt, med deilige italienske retter som pasta, pizza og gelato. Besøket til ikoniske steder som Duomo-katedralen og Leonardo da Vinci's Nattverden var uforglemmelig. Milano kombinerte perfekt moderne stil med historisk sjarm, og jeg forlot byen med uforglemmelige minner og en dypere kjærlighet for italiensk kultur.",
+    stars: 5,
+  },
+  {
+    dest: 'Shanghai',
+    text: 'Min tid i Shanghai var en utrolig opplevelse! Byen imponerte meg med sin blendende skyline og futuristiske arkitektur. Å utforske den pulserende gatenan og nattmarkedene ga meg et autentisk innblikk i den lokale kulturen. Shanghai Tower og The Bund var spektakulære, spesielt om natten når lysene skapte en fantastisk atmosfære. Maten var en smakfull reise med dim sum, dumplings og deilig street food. Jeg nøt også å utforske de historiske områdene som Yuyuan-hagen og Jade Buddha-tempelet. Shanghai kombinerte moderne livsstil med kulturell rikdom på en unik måte, og jeg forlot byen med en dypere forståelse og beundring for den pulserende metropolen.',
+    stars: 4,
+  },
+  {
+    dest: 'hhh',
+    text: 'minhkdjfu',
+    stars: 3,
+  },
+];
 
-
+const user = { name: 'Ola Nordmann', mail: 'ola@epost.no' };
 
 export default function Profile({
   params: { locale },
@@ -10,39 +27,47 @@ export default function Profile({
   params: { locale: string };
 }) {
   unstable_setRequestLocale(locale);
-
-
   return (
-    <div className='h-[150vh] mx-auto'> 
-    <Button size='sm' color='primary' variant='light' > Rediger </Button>
-    <h1 className="text-4xl font-bold text-left text-hvite-900 mt-8">Min profil</h1>
-    <div className="container justify-center mx-auto mt-8 p-4">
-      <div className="max-w-md bg-black shadow-md rounded-lg overflow-hidden">
-        <div className="flex items-left justify-start"> 
-        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-25 h-25 text-large" />
+    <div>
+      <div className='hidden shrink-0 py-2 sm:flex sm:flex-col sm:items-end'>
+        <Button as={Link} href='/profile/edit' variant='bordered'>
+          Rediger
+        </Button>
+      </div>
+      <div className='h-[150vh]'>
+        <h1 className='text-left text-4xl text-blue-500'>Min Profil</h1>
+        <div className='items-left flex justify-start py-4'>
+          <Avatar
+            src='https://i.pravatar.cc/150?u=a04258114e29026708c'
+            className='w-25 h-25 text-large'
+          />
         </div>
-        <div className="text-left mt-8 mb-8"> 
-          <h1 className="text-xl font-semibold text-blue-500">{"user.name"}</h1>{}
-          <p className="text-lg text-blue-700 mt-1">{"user.email"}</p>
-        </div>
-        <div className='mt-8 mb-8'></div>
-        <Divider/>
-          <h1 className="text-3xl font-bold text-left text-hvite-900 mt-8 mb-4">Mine vurderinger</h1>
-          <ul>
-            <li>Vurdering 1</li>
-            <li>Vurdering 2</li>
-            <li>Vurdering 3</li>
-          </ul>
+        <p>Brukernavn:</p>
+        <p>E-post:</p>
+
+        <br />
+
+        <h3 className='text-left text-2xl text-blue-500'>Mine vurderinger</h3>
+        <ul role='list' className='divide-y divide-gray-100'>
+          {reviews.map((reviews) => (
+            <li
+              key={reviews.text}
+              className='flex justify-between gap-x-6 py-5'
+            >
+              <div className='flex min-w-0 gap-x-4'>
+                <div className='min-w-0 flex-auto'>
+                  <p className='text-sm font-semibold leading-6'>
+                    {reviews.dest + '  ' + reviews.stars + '/5'}
+                  </p>
+                  <p className='mt-1 truncate text-xs leading-5 text-gray-500'>
+                    {reviews.text}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-    <div className="className='h-[150vh] mx-auto">
-    <Button size='sm' color='primary' variant='light'> Lagre </Button>
-    <h1 className="text-4xl font-bold mt-8 mb-4">Rediger profilen</h1>
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-            <Input type="name" label="Navn" defaultValue = "Ola Nordmann"></Input>
-        </div>
-    </div>
-  </div>
-
   );
 }
