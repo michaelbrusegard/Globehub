a6adfce3cae6a85d9caf692cba0112bccbebda7a
Rikke Schjonsby <rikkees@stud.ntnu.no>
2024-02-12 14:43:50 +0100

    feat: added profile avatar, review list and edit page to profile page. > > Co-authored-by: Martine Engebretsen <msengebr@stud.ntnu.no>

--- src/app/[locale]/(dashboard)/profile/edit/page.tsx
+++ src/app/[locale]/(dashboard)/profile/edit/page.tsx
@@ -1,19 +1,24 @@
+import { Avatar, Button, Input, Link } from '@nextui-org/react';
 
-import { Button, Input, Avatar } from '@nextui-org/react';
-
-export default function EditableProfile(){
-
+export default function EditableProfile() {
   return (
     <div className="className='h-[150vh] mx-auto">
-    <Button size='sm' color='primary' variant='light'> Lagre </Button>
-    <h1 className="text-4xl font-bold mt-8 mb-4">Rediger profilen</h1>
-    <div className="flex items-left justify-start"> 
-        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-25 h-25 text-large" />
+      <div className='hidden shrink-0 py-2 sm:flex sm:flex-col sm:items-end'>
+        <Button as={Link} href='/profile' variant='bordered'>
+          Lagre
+        </Button>
+      </div>
+      <h1 className='mb-4 mt-8 text-4xl font-bold'>Rediger profilen</h1>
+      <div className='items-left flex justify-start'>
+        <Avatar
+          src='https://i.pravatar.cc/150?u=a04258114e29026708c'
+          className='w-25 h-25 text-large'
+        />
 
-        <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-            <Input type="name" label="Navn" defaultValue = "Ola Nordmann"></Input>
+        <div className='flex w-full flex-wrap gap-4 md:flex-nowrap'>
+          <Input type='name' label='Navn' defaultValue='Ola Nordmann'></Input>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
