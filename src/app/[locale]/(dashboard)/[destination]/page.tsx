b783c10b59d0e03b6e91d24a48f67f27e3ca113b
Eivind Holmen <eivinbho@ntnu.no>
2024-02-12 16:28:22 +0100

    feat: Added google authentication button
    Added the front-end design of the google authenticator log-in button
    with google authentication logo.
    
    Issue Number: #06
    Co-authored-by: Nguyen, Kenneth

--- src/app/[locale]/(dashboard)/[destination]/page.tsx
+++ src/app/[locale]/(dashboard)/[destination]/page.tsx
@@ -1,14 +1,20 @@
-import { unstable_setRequestLocale } from 'next-intl/server';
+import React from 'react';
 
 export default function Destination({
   params: { locale },
 }: {
   params: { locale: string };
 }) {
-  unstable_setRequestLocale(locale);
   return (
-    <div>
-      <h1>Destinaion</h1>
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <button className='flex flex-nowrap items-center justify-center rounded bg-blue-500 p-6 text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50'>
+        <img
+          src='/authenticator.png'
+          alt='Google Authenticator'
+          className='mr-2 h-12 w-12'
+        />
+        Google Authenticator
+      </button>
     </div>
   );
 }
