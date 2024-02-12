e1fe15a2950e739cb91daf34c7c4143f161031c5
Eivind Holmen <eivinbho@ntnu.no>
2024-02-12 16:44:09 +0100

    feat: Changed the code to NextUI
    Switched to NextUI
    
    Issue Number: #06
    
    Co-authored-by: Nguyen, Kenneth <kenneng@stud.ntnu.no>

--- src/app/[locale]/(dashboard)/[destination]/page.tsx
+++ src/app/[locale]/(dashboard)/[destination]/page.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import { Button, Image } from '@nextui-org/react';
 
-export default function Destination({
-  params: { locale },
-}: {
-  params: { locale: string };
-}) {
+export default function LogIn({ params: { locale } }) {
   return (
-    <div className='flex h-screen flex-col items-center justify-center'>
-      <button className='flex flex-nowrap items-center justify-center rounded bg-blue-500 p-6 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50'>
-        <img
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Button color='primary' auto size='large' shadow>
+        <Image
           src='/authenticator.png'
           alt='Google Authenticator'
-          className='mr-2 h-12 w-12'
+          width={48}
+          height={48}
+          style={{ marginRight: '10px' }}
         />
         Google Authenticator
-      </button>
+      </Button>
     </div>
   );
 }
