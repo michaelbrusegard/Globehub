a168878fa400206cd0d8169cf2d458a983a0bf11
Michael Brusegard <michbrus@stud.ntnu.no>
2024-02-12 17:12:20 +0100

    feat(db): added local docker-compose for database

--- src/env.js
+++ src/env.js
@@ -7,7 +7,16 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_HOST: z.string(),
+    DATABASE_PORT: z
+      .string()
+      .transform(Number)
+      .refine((n) => !isNaN(n), {
+        message: 'DATABASE_PORT must be a valid number',
+      }),
+    DATABASE_USER: z.string(),
+    DATABASE_PASSWORD: z.string(),
+    DATABASE_NAME: z.string(),
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
       .default('development'),
@@ -31,7 +40,11 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_PORT: process.env.DATABASE_PORT,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,12 @@
 import { env } from '@/env';
 import postgres from 'postgres';
 
-const sql = postgres(env.DATABASE_URL, { ssl: 'require' });
+const sql = postgres({
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  database: env.DATABASE_NAME,
+  username: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+});
 
 export { sql };
