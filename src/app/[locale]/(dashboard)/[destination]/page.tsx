d517b06e4333e6ca7cf0a78dea5b72a92055b644
Eivind Holmen <eivinbho@ntnu.no>
2024-02-12 16:35:18 +0100

    feat: Added hover effect to the button Commit body - 72 characters ------------------------------------------| Added hover effect over the google authenticator button using Tailwind's hover variant.
    Issue Number: #06
    Co-authored-by: Nguyen, Kenneth

--- src/app/[locale]/(dashboard)/[destination]/page.tsx
+++ src/app/[locale]/(dashboard)/[destination]/page.tsx
@@ -7,7 +7,7 @@ export default function Destination({
 }) {
   return (
     <div className='flex h-screen flex-col items-center justify-center'>
-      <button className='flex flex-nowrap items-center justify-center rounded bg-blue-500 p-6 text-white focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50'>
+      <button className='flex flex-nowrap items-center justify-center rounded bg-blue-500 p-6 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50'>
         <img
           src='/authenticator.png'
           alt='Google Authenticator'
