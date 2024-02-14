8d9d8e14e6ba69803424a572dc9415ac6bee8d64
Martine Engebretsen <msengebr@stud.ntnu.no>
2024-02-14 10:06:57 +0100

    chore: Changed the names of the labels

--- src/app/[locale]/(dashboard)/profile/edit/page.tsx
+++ src/app/[locale]/(dashboard)/profile/edit/page.tsx
@@ -18,26 +18,26 @@ export default function EditableProfile() {
 
       <div>
         <label className='mb-2 block text-sm font-medium text-gray-900 dark:text-white'>
-          First name
+          Brukernavn
         </label>
         <input
           type='text'
           id='first_name'
           className='block w-64 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm font-semibold text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
-          placeholder='John'
+          placeholder='John Doe'
           required
         ></input>
       </div>
 
       <div className='mt-4'>
         <label className='mb-2 block text-sm font-medium text-gray-900 dark:text-white'>
-          Email
+          E-post
         </label>
         <input
           type='text'
           id='email'
           className='block w-64 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm font-semibold text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500'
-          placeholder='john@hotmail.com'
+          placeholder='john.doe@hotmail.com'
           required
         ></input>
       </div>
