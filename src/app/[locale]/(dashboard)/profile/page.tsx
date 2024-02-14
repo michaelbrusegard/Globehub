a119877e023bbe9a50740745171fe852d0539bdb
Michael Brusegard <michbrus@stud.ntnu.no>
2024-02-14 10:51:10 +0100

    chore(auth): add session from auth

--- src/app/[locale]/(dashboard)/profile/page.tsx
+++ src/app/[locale]/(dashboard)/profile/page.tsx
@@ -1,6 +1,9 @@
 import { Avatar, Button, Link } from '@nextui-org/react';
 import { unstable_setRequestLocale } from 'next-intl/server';
 
+import { auth } from '@/lib/auth';
+import { redirect } from '@/lib/navigation';
+
 const reviews = [
   {
     dest: 'Milano',
@@ -22,64 +25,66 @@ const reviews = [
   },
 ];
 
-const profilepic = 'https://i.pravatar.cc/150?u=a04258114e29026708c';
-
-const user = { name: 'Ola Nordmann', mail: 'ola@epost.no' };
-
-export default function Profile({
+export default async function Profile({
   params: { locale },
 }: {
   params: { locale: string };
 }) {
   unstable_setRequestLocale(locale);
-  return (
-    <div>
-      <div className='hidden shrink-0 py-2 sm:flex sm:flex-col sm:items-end'>
-        <Button as={Link} href='/profile/edit' variant='bordered'>
-          Rediger
-        </Button>
-      </div>
-      <div className='h-[150vh]'>
-        <h1 className='text-left text-4xl text-blue-500'>Min Profil</h1>
-        <div className='items-left flex justify-start py-4'>
-          <Avatar src={profilepic} className='w-25 h-25 text-large' />
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect('/');
+  } else {
+    return (
+      <div>
+        <div className='hidden shrink-0 py-2 sm:flex sm:flex-col sm:items-end'>
+          <Button as={Link} href='/profile/edit' variant='bordered'>
+            Rediger
+          </Button>
         </div>
-        <p>Brukernavn:</p>
-        <p>E-post:</p>
+        <div className='h-[150vh]'>
+          <h1 className='text-left text-4xl text-blue-500'>Min Profil</h1>
+          <div className='items-left flex justify-start py-4'>
+            <Avatar src={session.user.image!} size='lg' />
+          </div>
+          <p>Brukernavn: {session.user.name!}</p>
+          <p>E-post:</p>
 
-        <br />
+          <br />
 
-        <h3 className='text-left text-2xl text-blue-500'>Mine vurderinger</h3>
-        <ul role='list' className='divide-y divide-gray-100'>
-          {reviews.map((reviews) => (
-            <li
-              key={reviews.text}
-              className='flex justify-between gap-x-6 py-5'
-            >
-              <div className='flex min-w-0 gap-x-4'>
-                <img
-                  className='h-12 w-12 flex-none rounded-full bg-gray-50'
-                  src={profilepic}
-                  alt=''
-                />
-                <div className='min-w-0 flex-auto'>
-                  <p className='text-sm font-semibold leading-6'>
-                    {reviews.dest + '  ' + reviews.stars + '/5'}
-                  </p>
+          <h3 className='text-left text-2xl text-blue-500'>Mine vurderinger</h3>
+          <ul role='list' className='divide-y divide-gray-100'>
+            {reviews.map((reviews) => (
+              <li
+                key={reviews.text}
+                className='flex justify-between gap-x-6 py-5'
+              >
+                <div className='flex min-w-0 gap-x-4'>
+                  <img
+                    className='h-12 w-12 flex-none rounded-full bg-gray-50'
+                    src={session.user.image!}
+                    alt=''
+                  />
+                  <div className='min-w-0 flex-auto'>
+                    <p className='text-sm font-semibold leading-6'>
+                      {reviews.dest + '  ' + reviews.stars + '/5'}
+                    </p>
+                    <p className='mt-1 text-xs leading-5 text-gray-500'>
+                      {reviews.text}
+                    </p>
+                  </div>
+                </div>
+                <div className='hidden shrink-0 sm:flex sm:flex-col sm:items-end'>
                   <p className='mt-1 text-xs leading-5 text-gray-500'>
-                    {reviews.text}
+                    {reviews.date}
                   </p>
                 </div>
-              </div>
-              <div className='hidden shrink-0 sm:flex sm:flex-col sm:items-end'>
-                <p className='mt-1 text-xs leading-5 text-gray-500'>
-                  {reviews.date}
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
