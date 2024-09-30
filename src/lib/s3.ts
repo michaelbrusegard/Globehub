40a2df30718569f9325e4d5a7277be85d4e32af5
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-10-01 01:45:50 +0200

    chore: remove bucket policy from s3 client

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,31 +1,10 @@
 import { env } from '@/env';
 import {
   DeleteObjectCommand,
-  PutBucketPolicyCommand,
   PutObjectCommand,
   S3Client,
   type S3ClientConfig,
 } from '@aws-sdk/client-s3';
-
-async function setPublicBucketPolicy(bucketName: string) {
-  const params = {
-    Bucket: bucketName,
-    Policy: JSON.stringify({
-      Version: '2012-10-17',
-      Statement: [
-        {
-          Sid: 'PublicReadGetObject',
-          Effect: 'Allow',
-          Principal: '*',
-          Action: ['s3:GetObject'],
-          Resource: [`arn:aws:s3:::${bucketName}/*`],
-        },
-      ],
-    }),
-  };
-
-  await s3.send(new PutBucketPolicyCommand(params));
-}
 
 const endpoint: string = 'http://' + env.STORAGE_HOST + ':' + env.STORAGE_PORT;
 
@@ -45,11 +24,6 @@ const buckets = env.STORAGE_NAME.split(',');
 
 const destinationsBucket = buckets[0]!;
 const reviewsBucket = buckets[1]!;
-
-void (async () => {
-  await setPublicBucketPolicy(destinationsBucket);
-  await setPublicBucketPolicy(reviewsBucket);
-})();
 
 export {
   s3,
