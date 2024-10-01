53b4efe72f5ab6430a14047d805125944c67cb77
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-10-01 14:49:16 +0200

    fix: profile photo using storage url

--- src/components/destination/UserCard.tsx
+++ src/components/destination/UserCard.tsx
@@ -24,12 +24,7 @@ async function UserCard({ user }: { user: User }) {
     <Card shadow='none' className='max-w-[300px] border-none bg-transparent'>
       <CardHeader className='justify-between'>
         <div className='flex gap-3'>
-          <Avatar
-            isBordered
-            radius='full'
-            size='md'
-            src={env.NEXT_PUBLIC_STORAGE_URL + '/' + user.image}
-          />
+          <Avatar isBordered radius='full' size='md' src={user.image} />
           <div className='flex flex-col items-start justify-center'>
             <h4 className='text-small font-semibold leading-none text-default-600'>
               {user.name}
