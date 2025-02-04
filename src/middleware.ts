85405e4cfaaf10d9e982f8fb0c00ea1c6b333df8
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-02-04 17:17:33 +0100

    chore: fixup middleware to properly handle requests that should bypass i18n

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,24 @@
 import createMiddleware from 'next-intl/middleware';
+import { type NextRequest, NextResponse } from 'next/server';
 
-// import { auth } from '@/lib/auth';
 import { defaultLocale, localePrefix, locales, pathnames } from '@/lib/config';
 
-export default createMiddleware({
+const BYPASS_PATHS = ['/s3/', '/api/'] as const;
+
+const nextIntlMiddleware = createMiddleware({
   defaultLocale,
   localePrefix,
   locales,
   pathnames,
 });
 
-// export default auth((req) => {
-//   return intlMiddleware(req);
-// });
+export async function middleware(request: NextRequest) {
+  if (BYPASS_PATHS.some((path) => request.nextUrl.pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+  return nextIntlMiddleware(request);
+}
 
 export const config = {
-  matcher: ['/', '/no/:path*', '/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/', '/no/:path*', '/((?!_next|.*\\..*).*)'],
 };
