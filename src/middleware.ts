7e190a8e7209cc0ea6f1a86fb098b366f825dcb7
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-02-04 17:34:12 +0100

    chore: change to use matcher in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
-import { type NextRequest, NextResponse } from 'next/server';
 
 import { defaultLocale, localePrefix, locales, pathnames } from '@/lib/config';
 
-const BYPASS_PATHS = ['/s3/', '/api/'] as const;
-
-const nextIntlMiddleware = createMiddleware({
+export default createMiddleware({
   defaultLocale,
   localePrefix,
   locales,
   pathnames,
 });
 
-export async function middleware(request: NextRequest) {
-  if (BYPASS_PATHS.some((path) => request.nextUrl.pathname.startsWith(path))) {
-    return NextResponse.next();
-  }
-  return nextIntlMiddleware(request);
-}
-
 export const config = {
-  matcher: ['/', '/no/:path*', '/((?!_next|.*\\..*).*)'],
+  matcher: ['/', '/no/:path*', '/((?!_next|s3|api|.*\\..*).*)'],
 };
